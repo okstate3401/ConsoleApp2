7acaa33344298a5148b958dd283281eef6376d5d
okstate3401 <25288311+okstate3401@users.noreply.github.com>
2020-03-13 23:43:35 -0500

    added file

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Collections;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+
+            using (SpreadsheetDocument spreadSheetDocument = SpreadsheetDocument.Open(@"C:\\Users\\OkSta\\Downloads\\Personal\\Finances\\Balances.xlsx", false))
+            {
+
+                WorkbookPart workbookPart = spreadSheetDocument.WorkbookPart;
+                IEnumerable<Sheet> sheets = spreadSheetDocument.WorkbookPart.Workbook.GetFirstChild<Sheets>().Elements<Sheet>();
+                string relationshipId = sheets.First().Id.Value;
+                WorksheetPart worksheetPart = (WorksheetPart)spreadSheetDocument.WorkbookPart.GetPartById(relationshipId);
+                Worksheet workSheet = worksheetPart.Worksheet;
+                SheetData sheetData = workSheet.GetFirstChild<SheetData>();
+                IEnumerable<Row> rows = sheetData.Descendants<Row>();
+
+                foreach (Cell cell in rows.ElementAt(0))
+                {
+                    dt.Columns.Add(GetCellValue(spreadSheetDocument, cell));
+                }
+
+                foreach (Row row in rows) //this will also include your header row...
+                {
+                    DataRow tempRow = dt.NewRow();
+
+                    for (int i = 1; i < row.Descendants<Cell>().Count(); i++)
+                    {
+                        tempRow[i] = GetCellValue(spreadSheetDocument, row.Descendants<Cell>().ElementAt(i - 1));
+                    }
+
+                    dt.Rows.Add(tempRow);
+                }
+
+            }
+            dt.Rows.RemoveAt(0); //...so i'm taking it out here.
+            foreach (DataRow row in dt.Rows)
+            {
+                string name = row["Amount_Owed"].ToString();
+
+                Console.WriteLine(name);
+            }
+        }
+
+
+        public static string GetCellValue(SpreadsheetDocument document, Cell cell)
+        {
+            SharedStringTablePart stringTablePart = document.WorkbookPart.SharedStringTablePart;
+            string value = cell.CellValue.InnerXml;
+
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                return stringTablePart.SharedStringTable.ChildElements[Int32.Parse(value)].InnerText;
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
